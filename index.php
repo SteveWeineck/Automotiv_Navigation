1f817426bcc067f017a8e39cd41d375b27fb3a73
Steve Weineck <steve@gedankenstrich.net>
2019-10-02 17:45:39 +0200

    init

---
+++ index.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="wp-content/themes/navigation-automotiv/dist/css/style.css">
+    <script src="wp-content/themes/navigation-automotiv/dist/js/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<!--navigation-->
+
+
+
+<div id="top-bar" class="top-bar">    
+    <a id="logo" class="logo" href="#"><!-- link to home -->
+        <img class="logo-pos" src="wp-content/themes/navigation-automotiv/images/Logo_Automotive_Agile_PEP_Europe_pos.svg" alt="Logo" />
+        <img class="logo-neg" src="wp-content/themes/navigation-automotiv/images/Logo_Automotive_Agile_PEP_Europe_neg.svg" alt="Logo" />
+    </a>
+    <div class="top-bar_right">
+        <!-- Moible fixed Top Bar Start -->
+        <div class="mobile-fix-bar">
+            <div class="mobile-logo"> 
+                <img class="" src="wp-content/themes/navigation-automotiv/images/mobile-logo.svg" alr="logo" />
+            </div>
+            <div class="mobile-book-now">
+                <a href="#" >book now</a>
+            </div>
+            <div id="burger-menu-trigger" class="burger-menu-trigger">
+                <div class="burger-line burger-line1"></div>
+                <div class="burger-line burger-line2"></div>
+                <div class="burger-line burger-line3"></div>
+            </div>
+        </div>
+        <!-- Moible fixed Top Bar End -->
+        <div id="login-navigation" class="blue-top-bar">
+            <div class="blue-top-bar_left"><a href="#home" class="nodesktop"><img src="wp-content/themes/navigation-automotiv/images/home-icon.svg" alt="Logo" /><strong>Automotive Agile PEP Europe</strong>|</a>February 24 - 25, 2020 | San Francisco, USA</div><!-- Event Date & place -->
+            <a class="blue-top-bar_right login">
+                <svg xmlns="http://www.w3.org/2000/svg" width="9" height="9.037" viewBox="0 0 9 9.037"><g transform="translate(-657 -7.963)"><path d="M1127.5,6.5h3.019v8.037H1127.5" transform="translate(-465.019 1.963)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/><line x2="4" transform="translate(657.5 12.5)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="1"/><path d="M1131.5,10.5l2,2-2,2" transform="translate(-471)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="1"/></g></svg>
+                login
+            </a>
+        </div>
+        <div id="navigation" class="navigation">
+            <ul>
+                <li><a>Agenda</a>
+                    <ul>
+                        <li><a href="#">2020 Agenda<span>See all the exctiting Sessions at one glance</span></a></li>
+                        <li><a href="#">Award Dinner<span>The best Solutions of the yearin the Spotlight</span></a></li>
+                        <li><a href="#">Icebreaker<span>Get your Networking startetin style before the Event</span></a></li>     
+                        <li><a href="#">Speakers & Moderatores<span>This expectional Speakers are awaiting you this year</span></a></li>
+                        <li class="subnavi-highlight"><a href="#">become a Speaker</a></li><!--custom field checkbox that adds the class "subnavi-highlight" - the css puts the elements at the last position/order2-->
+                        <li class="subnavi-highlight"><a href="#">Nomine your Project</a></li>
+                        
+                    </ul>
+                </li>
+                <li><a>Partner</a>
+                     <ul>
+                        <li><a href="#">Partners 2020<span>See all the exctiting Sessions at one glance</span></a></li>
+                        <li><a href="#">Startups 2020<span>The best Solutions of the yearin the Spotlight</span></a></li>
+                        <li><a href="#">Media Partners 2020<span>Get your Networking startetin style before the Event</span></a></li> 
+                        <li class="subnavi-highlight"><a href="#">Partner with us</a></li>
+                        
+                    </ul>
+                </li>
+                <li><a>Who & Why</a>
+                <ul>
+                    <li><a href="#">2020 Agenda<span>See all the exctiting Sessions at one glance</span></a></li>
+                    <li><a href="#">Award Dinner<span>The best Solutions of the yearin the Spotlight</span></a></li>
+                    <li><a href="#">Icebreaker<span>Get your Networking startetin style before the Event</span></a></li>                        
+                    <li><a href="#">Icebreaker<span>Get your Networking startetin style before the Event</span></a></li>
+                    <li><a href="#">Speakers & Moderatores<span>This expectional Speakers are awaiting you this year</span></a></li>
+                    <li class="subnavi-highlight"><a href="#">become a Speaker</a></li>
+                    <li class="subnavi-highlight"><a href="#">2020 Agenda</a></li>                   
+                </ul>
+                </li>
+                <li><a>Event Info</a>
+                    <ul>
+                        <li><a href="#">2020 Agenda<span>See all the exctiting Sessions at one glance</span></a></li>
+                        <li><a href="#">Award Dinner<span>The best Solutions of the yearin the Spotlight</span></a></li>
+                        <li><a href="#">Icebreaker<span>Get your Networking startetin style before the Event</span></a></li>                        
+                        <li><a href="#">Icebreaker<span>Get your Networking startetin style before the Event</span></a></li>
+                        <li><a href="#">Speakers & Moderatores<span>This expectional Speakers are awaiting you this year</span></a></li>
+                        <li><a href="#">Speakers & Moderatores<span>This expectional Speakers are awaiting you this year</span></a></li>
+                        <li><a href="#">2020 Agenda<span>See all the exctiting Sessions at one glance</span></a></li>
+                        <li class="subnavi-highlight"><a href="#">2020 Agenda</a></li>  
+               
+                    </ul>
+                </li>
+                <li><a>Newsroom</a>
+                    <ul>
+                        <li><a href="#">2020 Agenda<span>See all the exctiting Sessions at one glance</span></a></li>
+                        <li><a href="#">Award Dinner<span>The best Solutions of the yearin the Spotlight</span></a></li>
+                        <li><a href="#">Icebreaker<span>Get your Networking startetin style before the Event</span></a></li>                        
+                        <li><a href="#">Icebreaker<span>Get your Networking startetin style before the Event</span></a></li>
+                        <li><a href="#">Speakers & Moderatores<span>This expectional Speakers are awaiting you this year</span></a></li>              
+                    </ul>
+                </li>
+                <li><a href="#" >book now</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<!-- content -->
+<div style="width: 100%; overflow-X: hidden;">
+    <div class="container-because-i-need-something-to-scroll" style="background:; height:40vh;">
+        <img src="wp-content/themes/navigation-automotiv/images/astrophotography-background-dark-746393.jpg" style="width: 100vw; object-fit: cover; height: 100vh;" alt="">
+    </div>
+    <div class="container-because-i-need-something-to-scroll" style="background:white; height:40vh;"></div>
+    <div class="container-because-i-need-something-to-scroll" style="background:grey; height:40vh;"></div>
+    <div class="container-because-i-need-something-to-scroll" style="background:white; height:40vh;"></div>
+    <div class="container-because-i-need-something-to-scroll" style="background:grey; height:40vh;"></div>
+    <div class="container-because-i-need-something-to-scroll" style="background:white; height:40vh;"></div>
+    <div class="container-because-i-need-something-to-scroll" style="background:grey; height:40vh;"></div>
+    <div class="container-because-i-need-something-to-scroll" style="background:white; height:40vh;"></div>
+</div>
+<!-- delete me end-->
+
+
+
+
+<script>
+//to force the column directed flexbox to wrap, the container needs a fixed height. so we give the box half of the height for 2 column layout. (I choose column instead of row to archive the wanted count direction)
+const subnvi = $( ".top-bar .top-bar_right .navigation ul ul" );
+
+var breakPointSwitch = 0; //because the windowWidth and css mediaquerie doesnt fit in all browsers, I check for an element, controlled by a media querie. So the css and JS funciton is fired always at the same time in all browsers.
+
+function curWid(){      
+    if($('.logo:visible').length != 0 && breakPointSwitch === 0 ) { 
+        breakPointSwitch = 1;        
+        //console.log(breakPointSwitch);
+            $('.top-bar .top-bar_right .navigation ul ul').each(function() {
+            var $this = $(this);
+            var halfSubnaviMenuHeight = $this.outerHeight()/2+40;
+                $this.css("height", halfSubnaviMenuHeight);
+            });
+    } else if($('.logo:visible').length == 0 && breakPointSwitch === 1 ) {
+        breakPointSwitch = 0;
+        //console.log(breakPointSwitch);
+        $(subnvi).css("height", "auto");
+    }
+}
+curWid();
+
+$( window ).resize(function() {
+    curWid();
+});
+
+/*mobile accordion stuff*/
+$('.navigation > ul > li').each(function() {
+    var $this = $(this);
+        $this.click( function(){
+            //$('.navigation > ul > li').removeClass('subnavi-active');
+            $('.navigation > ul > li').not($(this)).removeClass('subnavi-active');
+            $this.toggleClass('subnavi-active');
+        });
+});
+/*mobile accordion stuff ende*/
+
+//Burger Menu
+$('#burger-menu-trigger, .navigation ul ul a').click( function(){
+    $('#login-navigation').toggleClass('mobile-nav-active');
+    $('#navigation').toggleClass('mobile-nav-active');
+    $('#top-bar').toggleClass('top-bar-active');
+    $('#burger-menu-trigger').toggleClass('burger-active');
+});
+
+
+
+//toggleClass for topbar to change color after scrolling
+jQuery(document).ready(function($) {
+    $(window).on('scroll touchmove', function () {
+        $('#top-bar').toggleClass('nav-scroll-invert', $(document).scrollTop() == 0);
+    }).scroll();  
+});
+
+</script>
+ 
+</body>
+</html>
